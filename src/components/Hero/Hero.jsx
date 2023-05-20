128cce1c02d39149dc7eaafb0a9ec8fb199e269e
tarequemusa <tareque.web@gmail.com>
2023-05-20 08:28:48 +0600

    Client Readme.md file updated

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -10,7 +10,7 @@ const Hero = () => {
                     <h1 className="text-5xl text-green-800 font-bold">A house of Dolls & Dollhouses</h1>
                     <p className="text-2xl font-semibold my-2">Being a child is being happy... </p>
                     <div className="flex items-baseline justify-between">
-                        <img className="w-3/6" src="https://i.ibb.co/rb5RvRT/Chlidhood-Logo.png" alt="" />
+                        <img className="w-3/6 flex justify-center" src="https://i.ibb.co/rb5RvRT/Chlidhood-Logo.png" alt="" />
                         <Link to="/blogs"><button className="btn btn-primary">Explore our blog</button></Link>
                     </div>
                 </div>
